aad2c0e8f7bad6d7da4d1e68e20deadb81f41fe4
lize_bxbank <lize_lucky@163.com>
2022-02-10 23:11:10 +0800

    first commit

---
+++ rabbitmq/src/test/java/cn/itcast/rabbitmq/spring/MqDemo.java
@@ -0,0 +1,31 @@
+package cn.itcast.rabbitmq.spring;
+
+import com.rabbitmq.client.AMQP;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class MqDemo {
+
+    @Autowired
+    private RabbitTemplate amqpTemplate;
+
+    @Test
+    public void testSend() throws InterruptedException {
+        String msg = "hello, Spring boot amqp";
+        this.amqpTemplate.convertAndSend("spring.test.exchange","a.b", msg);
+
+        // 等待10秒后再结束
+        Thread.sleep(10000);
+    }
+}
